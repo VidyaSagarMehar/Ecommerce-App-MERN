a92048a7baa1de885d39e7fd66294da8fe30f5cc
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-02-24 00:45:21 +0530

    environment variable added

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+require('dotenv').config();
 const colors = require('colors');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 mongoose
-	.connect('mongodb://localhost:27017/ecommerce-app', {
+	.connect(process.env.MONGODB, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
-	.then((res) => console.log('> Connected...'.bgCyan))
+	.then((res) => console.log('> Connected...'.bgMagenta))
 	.catch((err) =>
 		console.log(
 			`> Error while connecting to mongoDB : ${err.message}`.underline.red,
