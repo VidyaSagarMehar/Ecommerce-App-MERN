c7126ef43fb40539ca142472772a101d13b9fd06
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-01 12:22:45 +0530

    get all users route and controller added

--- controllers/user.js
+++ controllers/user.js
@@ -15,9 +15,24 @@ exports.getUserById = (req, res, next, id) => {
 	});
 };
 
+// Getting the user
 exports.getUser = (req, res) => {
 	// To not send the salt and password value at the time of getting the user as json
 	req.profile.salt = undefined;
 	req.profile.encry_password = undefined;
 	return res.json(req.profile);
+};
+
+// getting all the users
+exports.getAllUsers = (req, res) => {
+	// if there is error
+	User.find().exec((err, users) => {
+		if (err || !users) {
+			return res.status(400).json({
+				error: 'No users found',
+			});
+		}
+		// if no error
+		res.json(users);
+	});
 };
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getUserById, getUser } = require('../controllers/user');
+const { getUserById, getUser, getAllUsers } = require('../controllers/user');
 const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth');
 
 // getting user as param from getUserById controller
 router.param('userId', getUserById);
 router.get('/user/:userId', isSignedIn, isAuthenticated, getUser);
+router.get('/users', getAllUsers);
 
 module.exports = router;
