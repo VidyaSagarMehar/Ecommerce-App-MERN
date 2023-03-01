f9b74156e1d2c73b4963185d75abaddbadc53ede
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-01 23:35:48 +0530

    pushOrderInPurchaseList controller added to recieve data from client and store in DB

--- controllers/user.js
+++ controllers/user.js
@@ -61,6 +61,7 @@ exports.updateUser = (req, res) => {
 };
 
 // controller to get user purchse list
+// pulling the info from the order model
 exports.userPurchaseList = (req, res) => {
 	Order.find({ user: req.profile._id })
 		.populate('user', '_id name') //reference documents in other collections
@@ -74,4 +75,36 @@ exports.userPurchaseList = (req, res) => {
 			// if no error, get all the orders of that user
 			return res.json(order);
 		});
+};
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+	let purchases = [];
+	req.body.order.products.forEach((product) => {
+		purchases.push({
+			_id: product._id,
+			name: product.name,
+			description: product.description,
+			category: product.category,
+			quantity: product.quantity,
+			amount: req.body.order.amount,
+			transaction_id: req.body.order.transaction_id,
+		});
+	});
+
+	// Store this in DB
+	// findOneAndUpdate() will update the array whether it is empty or not
+	User.findOneAndUpdate(
+		{ _id: req.profile._id },
+		{ $push: { purchases: purchases } }, //purchases inside user model
+		{ new: true }, //send me back updated one
+		(err, purchases) => {
+			// check for errors
+			if (err) {
+				return res.status(400).json({
+					error: 'Unable to save purchases list',
+				});
+			}
+			next();
+		},
+	);
 };
