6f763c8bc0a73a7e72fa6e1fc1d6edfaaa7d476f
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-01 12:08:41 +0530

    user controller modified

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,23 @@
 const User = require('../models/user');
 
+// getting the user by Id
 exports.getUserById = (req, res, next, id) => {
-	User.findById(id).exec(() => {
+	User.findById(id).exec((err, user) => {
+		// if there is error
 		if (err || !user) {
 			return res.status(400).json({
 				error: 'No user was found in DB',
 			});
 		}
+		// if no error
 		req.profile = user;
 		next();
 	});
 };
 
 exports.getUser = (req, res) => {
-	// TODO: for password
+	// To not send the salt and password value at the time of getting the user as json
+	req.profile.salt = undefined;
+	req.profile.encry_password = undefined;
 	return res.json(req.profile);
 };
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
 const { getUserById, getUser } = require('../controllers/user');
 const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth');
 
+// getting user as param from getUserById controller
 router.param('userId', getUserById);
 router.get('/user/:userId', isSignedIn, isAuthenticated, getUser);
 
