9642ee13d74c8d9e256df64d01c17a7920c1a7ff
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-02 19:56:55 +0530

    createProduct controller created with formidable, lodash and file system

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require('../models/product');
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs'); //file system
 
 exports.getProductById = (req, res, next, id) => {
 	Product.findById(id)
@@ -14,4 +17,44 @@ exports.getProductById = (req, res, next, id) => {
 			req.product = product;
 			next();
 		});
+};
+
+exports.creataProduct = (req, res) => {
+	let form = new formidable.IncomingForm();
+	form.keepExtensions = true;
+
+	form.parse(req, (err, fields, file) => {
+		// check for error
+		if (err) {
+			return res.status(400).json({
+				error: 'Problem with image',
+			});
+		}
+		// if no error
+		// TODO: restriction on field
+		let product = new Product(fields);
+
+		// handle file here
+		if (file.photo) {
+			if (file.photo.size > 3000000) {
+				return res.status(400).json({
+					error: 'file size too big!',
+				});
+			}
+			product.photo.data = fs.readFileSync(file.photo.path);
+			product.photo.contentType = file.photo.type;
+		}
+
+		// save to db
+		product.save((err, product) => {
+			// check for error
+			if (err) {
+				return res.status(400).json({
+					error: 'Saving tshirt in db failed',
+				});
+			}
+			// if no error
+			res.json(product);
+		});
+	});
 };
