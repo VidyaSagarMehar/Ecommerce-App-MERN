93d25cb5eb5e7d632b9d5a5e4491b78b1c00b4c6
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-02 11:16:50 +0530

    create category route and controller added

--- controllers/category.js
+++ controllers/category.js
@@ -13,3 +13,17 @@ exports.getCategoryById = (req, res, next, id) => {
 		next();
 	});
 };
+
+exports.createCategory = (req, res) => {
+	const category = new Category(req.body);
+	category.save((err, category) => {
+		// check for error
+		if (err) {
+			return req.status(400).json({
+				error: 'Category creation failed',
+			});
+		}
+		// if no error
+		res.json({ category });
+	});
+};
