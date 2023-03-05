febf6e000926311ba06d8dc0335a9fc8884d3285
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-05 23:11:17 +0530

    Connected client to server

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles.css';
+import { API } from '../backend';
 
 const Home = () => {
+	console.log('API IS', API);
 	return (
 		<div>
-			<h1>Hello frontend</h1>
+			<h1 className="text-white">Hello frontend</h1>
 		</div>
 	);
 };
