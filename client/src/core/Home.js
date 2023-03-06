62d364c3a8cf235ddb08feb6c02e06fbb481e9b7
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-06 23:36:27 +0530

    singin | Signup form added in component

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -6,7 +6,7 @@ import Base from './Base';
 const Home = () => {
 	console.log('API IS', API);
 	return (
-		<Base title="Home Page">
+		<Base title="Home Page" description="Home page of app">
 			<h1 className="text-white">Hello frontend</h1>
 		</Base>
 	);
