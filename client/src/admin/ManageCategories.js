a645b7fec12cd11a85b5b23252fcb16300c80f0b
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-12 22:39:03 +0530

    Managecategories component added with handlers

--- client/src/admin/ManageCategories.js
+++ client/src/admin/ManageCategories.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Base from '../core/Base';
+import { isAuthenticated } from '../auth/helper';
+import { getCategories } from './helper/adminapicall';
 
 const ManageCategories = () => {
+	// All the states
+	const [categories, setCategories] = useState([]);
+
+	// getting user and token from isAuthenticated and destructure them
+	const { user, token } = isAuthenticated();
+
+	// preload the products from getProducts method of 'adminapicall'
+	const preload = () => {
+		getCategories()
+			.then((data) => {
+				if (data.error) {
+					console.log(data.error);
+				} else {
+					setCategories(data);
+				}
+			})
+			.catch((err) => console.log(err));
+	};
+
+	useEffect(() => {
+		preload();
+	}, []);
 	return (
-		<Base>
-			<h1 className="text-white">Assignment</h1>
+		<Base title="Welcome admin" description="Manage products here">
+			<h2 className="mb-4">All products:</h2>
+			<Link className="btn btn-info" to={`/admin/dashboard`}>
+				<span className="">Admin Home</span>
+			</Link>
+			<div className="row">
+				<div className="col-12">
+					<h2 className="text-center text-white my-3">Total 3 products</h2>
+					{categories.map((category, index) => {
+						return (
+							<h3 className="text-white" key={index}>
+								{category.name}
+							</h3>
+						);
+					})}
+					<div className="row text-center mb-2 ">
+						<div className="col-4">
+							<h3 className="text-white text-left">I write code</h3>
+						</div>
+						<div className="col-4">
+							<Link
+								className="btn btn-success"
+								to={`/admin/product/update/productId`}
+							>
+								<span className="">Update</span>
+							</Link>
+						</div>
+						<div className="col-4">
+							<button onClick={() => {}} className="btn btn-danger">
+								Delete
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</Base>
 	);
 };
