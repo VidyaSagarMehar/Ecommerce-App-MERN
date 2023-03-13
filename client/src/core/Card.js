daa2294152ab90744458630c7bed534a65897f80
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-13 22:44:49 +0530

    conditional methods to show addto cart and remove from cart button added

--- client/src/core/Card.js
+++ client/src/core/Card.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import ImageHelper from './helper/ImageHelper';
 
-const Card = ({ product }) => {
+const Card = ({ product, addToCart = true, removeFromCart = false }) => {
+	// Conditional rendering of add to cart and remove from cart button
+	// Method to show add to cart button
+	const showAddToCart = (addToCart) => {
+		return (
+			addToCart && (
+				<button
+					onClick={() => {}}
+					className="btn btn-block btn-outline-success mt-2 mb-2"
+				>
+					Add to Cart
+				</button>
+			)
+		);
+	};
+
+	// Method to show remove from cart button (By default it will not shown up)
+	const showRemoveFromCart = (removeFromCart) => {
+		return (
+			removeFromCart && (
+				<button
+					onClick={() => {}}
+					className="btn btn-block btn-outline-danger mt-2 mb-2"
+				>
+					Remove from cart
+				</button>
+			)
+		);
+	};
+
 	return (
 		<div className="card text-white bg-dark border border-info ">
 			<div className="card-header lead">A photo from pexels</div>
@@ -12,22 +41,8 @@ const Card = ({ product }) => {
 				</p>
 				<p className="btn btn-success rounded  btn-sm px-4">$ 5</p>
 				<div className="row">
-					<div className="col-12">
-						<button
-							onClick={() => {}}
-							className="btn btn-block btn-outline-success mt-2 mb-2"
-						>
-							Add to Cart
-						</button>
-					</div>
-					<div className="col-12">
-						<button
-							onClick={() => {}}
-							className="btn btn-block btn-outline-danger mt-2 mb-2"
-						>
-							Remove from cart
-						</button>
-					</div>
+					<div className="col-12">{showAddToCart(addToCart)}</div>
+					<div className="col-12">{showRemoveFromCart(removeFromCart)}</div>
 				</div>
 			</div>
 		</div>
