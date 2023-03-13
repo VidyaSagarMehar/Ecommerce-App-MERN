96c4e4dc2df6ce11d61cf3c42c72145a4374f9d5
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-13 22:32:51 +0530

    Card and imageHelper component added

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -2,12 +2,22 @@ import React from 'react';
 import '../styles.css';
 import { API } from '../backend';
 import Base from './Base';
+import Card from './Card';
 
 const Home = () => {
-	// console.log('API IS', API);
 	return (
 		<Base title="Home Page" description="Home page of app">
-			<h1 className="text-white">Hello frontend</h1>
+			<div className="row text-center">
+				<div className="col-4">
+					<Card />
+				</div>
+				<div className="col-4">
+					<button className="btn btn-success">TEST</button>
+				</div>
+				<div className="col-4">
+					<button className="btn btn-success">TEST</button>
+				</div>
+			</div>
 		</Base>
 	);
 };
