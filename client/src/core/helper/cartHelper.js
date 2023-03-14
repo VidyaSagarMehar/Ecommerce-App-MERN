caaa6a301e22a9997d17919dd74ee6bf13cbcf39
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-14 22:45:38 +0530

    remove from cart functionality added

--- client/src/core/helper/cartHelper.js
+++ client/src/core/helper/cartHelper.js
@@ -22,3 +22,20 @@ export const loadCart = () => {
 		}
 	}
 };
+
+// helper method to remove item from cart
+export const removeItemFromCart = (productId) => {
+	let cart = [];
+	if (typeof window !== undefined) {
+		if (localStorage.getItem('cart')) {
+			cart = JSON.parse(localStorage.getItem('cart'));
+		}
+		cart.map((product, index) => {
+			if (product._id === productId) {
+				cart.splice(index, 1);
+			}
+		});
+		localStorage.setItem('cart', JSON.stringify(cart));
+	}
+	return cart;
+};
