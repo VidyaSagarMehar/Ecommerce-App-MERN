e263092463469d777e497b34535f8aa257df39cf
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-03-17 19:37:33 +0530

    stripe payment getway integrated

--- client/src/core/StripeCheckout.js
+++ client/src/core/StripeCheckout.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { isAuthenticated } from '../auth/helper';
 import { cartEmpty, loadCart } from './helper/cartHelper';
+import StripeCheckoutButton from 'react-stripe-checkout';
+import { API } from '../backend';
+import { createOrder } from './helper/orderHelper';
 
 const StripeCheckout = ({
 	products,
@@ -29,10 +32,43 @@ const StripeCheckout = ({
 		return price;
 	};
 
+	//
+	const makePayment = (token) => {
+		const body = {
+			token,
+			products,
+		};
+		const headers = {
+			'Content-Type': 'application/json',
+		};
+		return fetch(`${API}/stripepayment`, {
+			method: 'POST',
+			headers,
+			body: JSON.stringify(body),
+		})
+			.then((response) => {
+				console.log(response);
+				// can call furthur method here
+				const { status } = response;
+				console.log('STATUS', status);
+				cardEmpty();
+			})
+			.catch((err) => console.log(err));
+	};
+
 	// conditional rendering of checkout button
 	const showStripeButton = () => {
 		return isAuthenticated() ? (
-			<button className="btn btn-success">Stripe Checkout</button>
+			<StripeCheckoutButton
+				stripeKey="pk_test_51Mly4CSFWhgOaplCG5Dxx92vIX4nPkyPzDNGNWpumRgv1H2oOTCPi6taZMfvc7ZoXpTUMlEzsXPbc1BkIQUrSivr00iZlNGjMG"
+				token={makePayment}
+				amount={getFinalPrice() * 100}
+				name="Buy T shirt"
+				// shippingAddress
+				// billingAddress
+			>
+				<button className="btn btn-success">Stripe Checkout</button>
+			</StripeCheckoutButton>
 		) : (
 			<Link to="/signin">
 				<button className="btn btn-warning">Signin</button>
--- server/controllers/stripePayment.js
+++ server/controllers/stripePayment.js
@@ -1,4 +1,6 @@
-const stripe = require('stripe')('secret_key');
+const stripe = require('stripe')(
+	'sk_test_51Mly4CSFWhgOaplCTTrIjaJE4hnZZlG3MsKFXlFs9rYNLYTGSeUQEbVSgfnomcDlmxieEuoSvwOLk8gZsCZgwFed00oEKOXiYM',
+);
 const { result } = require('lodash');
 const { v4: uuid } = require('uuid');
 
@@ -26,12 +28,20 @@ exports.makepayment = (req, res) => {
 		.then((customer) => {
 			stripe.charges.create(
 				{
-					amount: amount,
+					amount: amount * 100,
 					currency: 'usd',
 					customer: customer.id,
 					receipt_email: token.email,
+					description: 'a test account',
 					shipping: {
 						name: token.card.name,
+						address: {
+							line1: token.card.address_line1,
+							line2: token.card.address_line2,
+							city: token.card.address_city,
+							country: token.card.address_country,
+							postal_code: token.card.address_zip,
+						},
 					},
 				},
 				{ idempotencyKey },
